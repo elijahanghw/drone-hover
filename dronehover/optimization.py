e163611f3dfa5bbbcc2c24325cb40452f72210f1
Elijah <elijah.anghw@gmail.com>
2024-07-15 10:16:48 +0200

    Update optim parameters

--- dronehover/optimization.py
+++ dronehover/optimization.py
@@ -106,7 +106,7 @@ class Hover:
         bnds = []
         for i in range(self.control_limits.shape[0]):
             bnds.append((self.w_hat_bounds[0]**2, self.w_hat_bounds[1]**2)) 
-        opt = {'maxiter':200}
+        opt = {'maxiter':1000}
         
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", message="Values in x were outside bounds")
@@ -178,7 +178,7 @@ class Hover:
         bnds = []
         for i in range(self.control_limits.shape[0]):
             bnds.append((self.w_hat_bounds[0]**2, self.w_hat_bounds[1]**2)) 
-        opt = {'maxiter':500, 'ftol':5e-3}
+        opt = {'maxiter':1000, 'ftol':5e-3}
         
         with warnings.catch_warnings():
             warnings.filterwarnings("ignore", message="Values in x were outside bounds")
