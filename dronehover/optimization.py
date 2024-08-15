76c3358ce959f141e855bcd38c5299132ebd39a2
Elijah <elijah.anghw@gmail.com>
2024-08-15 13:25:03 +0200

    Update initial conditions

--- dronehover/optimization.py
+++ dronehover/optimization.py
@@ -88,8 +88,8 @@ class Hover:
         A = self.Bf.T @ self.Bf
         
         # Defining eta as a shorthand (eta = w_hat**2)
-        # eta0 = np.random.uniform(low=self.w_hat_bounds[0]**2, high=self.w_hat_bounds[1]**2, size=self.num_props)
-        eta0 = 0.5*np.ones(self.num_props)
+        eta0 = np.random.uniform(low=self.w_hat_bounds[0]**2, high=self.w_hat_bounds[1]**2, size=self.num_props)
+        # eta0 = 0.5*np.ones(self.num_props)
         
         def objective_function(eta):
             return eta.T @ eta
@@ -157,8 +157,8 @@ class Hover:
         
         # Defining eta as a shorthand (eta = u**2)
         # Somehow if values of u are all equal it does not work
-        # eta0 = np.random.uniform(low=self.w_hat_bounds[0]**2, high=self.w_hat_bounds[1]**2, size=self.num_props)
-        eta0 = 0.5*np.ones(self.num_props)
+        eta0 = np.random.uniform(low=self.w_hat_bounds[0]**2, high=self.w_hat_bounds[1]**2, size=self.num_props)
+        # eta0 = 0.5*np.ones(self.num_props)
         
         def objective_function(eta):
             return eta.T @ eta
