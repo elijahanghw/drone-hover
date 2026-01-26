28bb9f734c662a4919c7e2d1982ff91c567fb6db
Elijah Ang <elijah.anghw@gmail.com>
2026-01-26 16:09:03 +0100

    Update carbon tube density

--- dronehover/bodies/custom_bodies.py
+++ dronehover/bodies/custom_bodies.py
@@ -36,7 +36,7 @@ class Custombody:
     def get_inertia(self):
         controller_mass = 0.0136 # speedybee f405 aio
         battery_mass = 0.043 # 3s 450mah lipo
-        beam_density = 1500*0.005*0.01 # kg/m, carbon fiber plates, 5mm thickness, 10mm width
+        beam_density = 0.034 # kg/m, carbon fiber plates, 5mm thickness, 10mm width
 
         self.mass = controller_mass + battery_mass
 
